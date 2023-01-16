1821c600127825ffade70659b5799580fd78c6a7
yunusemre.emik <yunusemre.emik@logo.com.tr>
2023-01-17 00:50:26 +0300

    final version

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 # setup.py
 
 from setuptools import setup
-
+long_description = ""
+"""
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ """
 setup(
-    name='tfidf_transformer',
+    name='tfidf_transformer_afiniti',
     version='0.1',
     description='A package for performing TF-IDF transformation on text data',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/username/tfidf_transformer',
-    packages=['tfidf_transformer'],
+    author='Yunus Emre Emik',
+    author_email='yunus_emre3497@hotmail.com',
+    url='https://github.com/yunusemreemik/tfidf_afiniti.git',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['tfidf_transformer_afiniti'],
     install_requires=[
-        'sklearn',
+        'scikit-learn',
     ],
 )
 
